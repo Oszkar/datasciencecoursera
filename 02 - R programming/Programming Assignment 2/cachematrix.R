0e88a19572e09f19543f8a738c93f30c1448c91c
Oszkar Jozsa <jozsa.oszkar@gmail.com>
2015-01-17 18:41:28 +0100

    Adding programming assignment to 02

---
+++ 02 - R programming/Programming Assignment 2/cachematrix.R
@@ -0,0 +1,32 @@
+## Assignment 2: caching the inverse of the matrix
+## with the following functions we are able to create a special matrix
+## that caches it's inverse and only calculates it, when the matrix has changed
+
+## Creates our special matrix with the inverse getter/setter
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+
+## Performs the cached inverse. If there's a cached one, it does not calculate again
+cacheSolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+}
